422d765f393f0b5c6f50e9b56347b69d793fae1f
McNester <seraf5@bk.ru>
2021-08-12 18:59:34 +0600

    IC

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express'); //requires express module
+const socket = require('socket.io'); //requires socket.io module
+const fs = require('fs');
+const app = express();
+var PORT = process.env.PORT || 5000;
+const server = app.listen(PORT); //tells to host server on localhost:3000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+//Playing variables:
+app.use(express.static('public')); //show static files in 'public' directory
+console.log('Server is running');
+const io = socket(server);
+
+var count = 0;
+
+
+//Socket.io Connection------------------
+io.on('connection', (socket) => {
+
+    console.log("New socket connection: " + socket.id)
+
+    socket.on('counter', () => {
+        count++;
+        console.log(count)
+        io.emit('counter', count);
+    })
+})
