e4c78a742fe95a1768aee2585fa0965c0127129b
McNester <seraf5@bk.ru>
2021-08-22 10:11:07 +0600

    cleared index

--- index.js
+++ index.js
@@ -11,41 +11,11 @@ app.use(express.static('public')); //show static files in 'public' directory
 console.log('Server is running');
 const io = socket(server);
 
-//Naziya
 
-
-//ewf
 //Socket.io Connection------------------
 io.on('connection', (socket) => {
 
     console.log("New socket connection: " + socket.id)
 
-    socket.on('messagedetection', (messageContent,roomName) => {
-
-       //log the message in console
-
-       console.log("Message: "+messageContent)
-
-      //create a message object
-
-      let  message = {"message":messageContent}
-
-      // send the message to all users including the sender  using io.emit()
-      io.to(roomName).emit('message', message )
-
-
-      })
-
-      socket.on('joinRoom', (roomName) =>{
-
-        socket.join(roomName);
-
-        console.log(" New user has joined "+roomName + "!")
-
-        let  message = {"message": " New user has joined "}
-
-        io.to(roomName).emit('newUser', message )
-
-      })
-//hello world
+    
 });
