54ef5424129ae56e990bc365fa8f0897aec1b436
McNester <seraf5@bk.ru>
2021-08-22 21:30:05 +0600

    returned broadcast

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ io.on('connection',(socket)=>{
   socket.on("roomDetection",(roomStr)=>{
     console.log("The new room has been created");
     let roomName = {"roomName": roomStr}
-    socket.emit("newRoom",  roomName);
+    socket.broadcast.emit("newRoom",  roomName);
   })
 })
