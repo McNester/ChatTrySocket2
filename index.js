acbaaa5ae18b049ab0e3ab0862902c28cb9101df
McNester <seraf5@bk.ru>
2021-08-22 11:23:13 +0600

    PLEASE WORK

--- index.js
+++ index.js
@@ -11,20 +11,7 @@ app.use(express.static('public')); //show static files in 'public' directory
 console.log('Server is running');
 const io = socket(server);
 
-//hello world!
-
-//is it work? naziya
-
-//fw
-//Socket.io Connection------------------
-io.on('connection', (socket) => {
-<<<<<<< HEAD
-
-    console.log("New socket connection- " + socket.id)
-
-
-=======
-    console.log("New socket connection: " + socket.id)
->>>>>>> 26e64b3e051b30b22ad9803bd9b7955b58e6e6b5
+// Socket.io Connection------------------
+io.on('connection',(socket)=>{
+  console.log("New socket conntection---- " + socket.id)
 });
-//pihou
